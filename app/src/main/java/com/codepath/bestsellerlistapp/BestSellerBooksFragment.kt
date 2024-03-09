e41a725a5c6de979fd3ccaf26f9617b55aec1a2a
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2024-03-09 11:38:21 -0800

    Completed lab

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksFragment.kt
@@ -1,6 +1,7 @@
 package com.codepath.bestsellerlistapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,12 +10,20 @@ import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.RequestParams
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.codepath.asynchttpclient.callback.TextHttpResponseHandler
 import com.codepath.bestsellerlistapp.R
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Headers
+import org.json.JSONObject
 
 // --------------------------------//
 // CHANGE THIS TO BE YOUR API KEY  //
 // --------------------------------//
-private const val API_KEY = "<YOUR-API-KEY-HERE>"
+private const val API_KEY = "9OA648ydy3tk88DOHs8OrOvxaHVGarRk"
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -46,31 +55,46 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
         progressBar.show()
 
         // Create and set up an AsyncHTTPClient() here
+        val client = AsyncHttpClient()
+        val params = RequestParams()
 
         // Using the client, perform the HTTP request
+        params["limit"] = "5"
+        params["page"] = "0"
+        params["api-key"] = API_KEY
 
-        /* Uncomment me once you complete the above sections!
+        client["https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json", params, object :
+            JsonHttpResponseHandler()
+
         {
             /*
              * The onSuccess function gets called when
              * HTTP response status is "200 OK"
              */
-            override fun onSuccess(
-                statusCode: Int,
-                headers: Headers,
-                json: JsonHttpResponseHandler.JSON
-            ) {
-                // The wait for a response is over
+            // Inside your onSuccess function
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
                 progressBar.hide()
 
-                //TODO - Parse JSON into Models
+                try {
+                    json?.let {
+                        val gson = Gson()
+                        val resultsJSON = it.jsonObject.getJSONObject("results")
+                        val booksJSONArray = resultsJSON.getJSONArray("books")
 
-                val models : List<BestSellerBook> = null // Fix me!
-                recyclerView.adapter = BestSellerBooksRecyclerViewAdapter(models, this@BestSellerBooksFragment)
+                        val bookListType = object : TypeToken<List<BestSellerBook>>() {}.type
+                        val models: List<BestSellerBook> = gson.fromJson(booksJSONArray.toString(), bookListType)
 
-                // Look for this in Logcat:
-                Log.d("BestSellerBooksFragment", "response successful")
+                        recyclerView.adapter = BestSellerBooksRecyclerViewAdapter(models, this@BestSellerBooksFragment)
+
+                        Log.d("BestSellerBooksFragment", "response successful")
+                    }
+                } catch (e: Exception) {
+                    Log.e("BestSellerBooksFragment", "Failed to parse JSON", e)
+                }
             }
+
+
+
 
             /*
              * The onFailure function gets called when
@@ -91,8 +115,6 @@ class BestSellerBooksFragment : Fragment(), OnListFragmentInteractionListener {
                 }
             }
         }]
-        */
-
     }
 
     /*
--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.kt
@@ -1,11 +1,18 @@
 package com.codepath.bestsellerlistapp
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.codepath.bestsellerlistapp.R.id
+import org.w3c.dom.Text
 
 /**
  * [RecyclerView.Adapter] that can display a [BestSellerBook] and makes a call to the
@@ -31,6 +38,11 @@ class BestSellerBooksRecyclerViewAdapter(
         var mItem: BestSellerBook? = null
         val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
         val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
+        val mBookRanking: TextView = mView.findViewById<View>(id.ranking) as TextView
+        val mBookDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
+        val mBookImage: ImageView = mView.findViewById<View>(id.book_image) as ImageView
+        val mBookButton: Button = mView.findViewById<Button>(id.buy_button) as Button
+
 
         override fun toString(): String {
             return mBookTitle.toString() + " '" + mBookAuthor.text + "'"
@@ -46,6 +58,17 @@ class BestSellerBooksRecyclerViewAdapter(
         holder.mItem = book
         holder.mBookTitle.text = book.title
         holder.mBookAuthor.text = book.author
+        holder.mBookRanking.text = book.rank.toString()
+        holder.mBookDescription.text = book.description
+        Glide.with(holder.mView)
+            .load(book.bookImageUrl)
+            .centerInside()
+            .into(holder.mBookImage)
+
+        holder.mBookButton.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(book.amazonUrl))
+            startActivity(it.context, browserIntent, null)
+        }
 
         holder.mView.setOnClickListener {
             holder.mItem?.let { book ->
